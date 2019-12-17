6c709c35bf8a55119560befd4a1a426ce68cb4e1
yuekun1 <1999Fred/>
2019-12-17 12:51:41 -0800

    fuck you java list and array

--- src/Model/AnimeCrawler.java
+++ src/Model/AnimeCrawler.java
@@ -12,7 +12,23 @@ public class AnimeCrawler {
 
     private String input;
     private final String NYAA_SEARCH_URL = "https://nyaa.si/?f=0&c=0_0&q=";
-    private final String NYAA_VIEW_URL = "https://nyaa.si";
+    private final String NYAA_DOWNLOAD_URL = "https://nyaa.si";
+
+
+    /**
+     * SPEC about the String array array
+     * each string array contains the info of the source
+     * 0 the detailed site page on nyaa.si
+     * 1 name of the torrent
+     * 2 magnet value
+     * 3 size of the files
+     * 4 date of created
+     * 5 number of seeders
+     * 6 number of leechers
+     * 7 count of completion
+     * */
+
+    private String[][] arrays = new String[1000][];
 
     public AnimeCrawler(String input) {
         this.input = inputParser(input);
@@ -44,20 +60,30 @@ public class AnimeCrawler {
 
             Elements tableHeaderEles = tableElements.select("thead tr th");
             for (int i = 0; i < tableHeaderEles.size(); i++) {
-                System.out.println(tableHeaderEles.get(i).text());
+                System.out.println("@!@#!@#!@#!#!#" + tableHeaderEles.get(i).text());
             }
             System.out.println();
 
             Elements tableRowElements = tableElements.select(":not(thead) tr");
 
-            for (int i = 0; i < tableRowElements.size(); i++) {
+            for (int i = 1; i < tableRowElements.size(); i++) {
                 Element row = tableRowElements.get(i);
-                System.out.println("row" + i);
+                System.out.println("row@ " + i);
                 Elements rowItems = row.select("td");
                 System.out.println(rowItems.size());
+                String[] eachResult = new String[8];
                 for (int j = 0; j < rowItems.size(); j++) {
-                    System.out.println(rowItems.get(j).text());
+                    // System.out.println(rowItems.get(j).text());
+                    eachResult[j] = rowItems.get(j).text();
                 }
+                // System.out.println(rowItems.get(2).select("a").get(1).attr("href").split("&")[0]);
+                eachResult[0] = NYAA_DOWNLOAD_URL + rowItems.get(1).select("a").attr("href");
+                eachResult[2] = rowItems.get(2).select("a").get(1).attr("href").split("&")[0];
+                for (String string :
+                        eachResult) {
+                    System.out.println(string);
+                }
+                arrays[i] = eachResult;
             }
 
         } catch (IOException e) {
