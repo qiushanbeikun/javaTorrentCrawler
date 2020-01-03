de99a54e76d25985dddccf833319bce0b4517a5d
qiushanbeikun <beikuncanada@gmail.com>
2020-01-03 03:39:51 -0800

    whats wrong with fucking java?

--- src/Model/AnimeCrawler.java
+++ src/Model/AnimeCrawler.java
@@ -11,8 +11,6 @@ import java.util.ArrayList;
 public class AnimeCrawler extends Crawler{
 
     private String input;
-    private final String NYAA_SEARCH_URL = "https://nyaa.si/?f=0&c=0_0&q=";
-    private final String NYAA_DOWNLOAD_URL = "https://nyaa.si";
 
 
     /**
@@ -20,7 +18,7 @@ public class AnimeCrawler extends Crawler{
      * each string array contains the info of the source
      * 0 the detailed site page on nyaa.si
      * 1 name of the torrent
-     * 2 magnet value
+     * 2 magnet hash value
      * 3 size of the files
      * 4 date of created
      * 5 number of seeders
@@ -28,7 +26,7 @@ public class AnimeCrawler extends Crawler{
      * 7 count of completion
      * */
 
-    private String[][] arrays = new String[1000][];
+    private String[][] arrays;
 
     public AnimeCrawler(String input) {
         this.input = inputParser(input);
@@ -37,6 +35,7 @@ public class AnimeCrawler extends Crawler{
 
     public String startSearch () {
 
+        String NYAA_SEARCH_URL = "https://nyaa.si/?f=0&c=0_0&q=";
         String url = NYAA_SEARCH_URL + this.input + "&s=size&o=desc";
 
         try {
@@ -51,6 +50,7 @@ public class AnimeCrawler extends Crawler{
 
             Elements tableRowElements = tableElements.select(":not(thead) tr");
 
+            arrays = new String[tableRowElements.size()][];
             for (int i = 1; i < tableRowElements.size(); i++) {
                 Element row = tableRowElements.get(i);
                 System.out.println("row@ " + i);
@@ -62,6 +62,7 @@ public class AnimeCrawler extends Crawler{
                     eachResult[j] = rowItems.get(j).text();
                 }
                 // System.out.println(rowItems.get(2).select("a").get(1).attr("href").split("&")[0]);
+                String NYAA_DOWNLOAD_URL = "https://nyaa.si";
                 eachResult[0] = NYAA_DOWNLOAD_URL + rowItems.get(1).select("a").attr("href").split("#")[0];
                 eachResult[2] = rowItems.get(2).select("a").get(1).attr("href").split("&")[0];
                 for (String string :
@@ -74,18 +75,39 @@ public class AnimeCrawler extends Crawler{
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return animeSelector(arrays);
 
     }
 
     /**
      * @param resultList the string array array containing the search result
-     * @return a string in form of content^content^content
-     * content form: contentCharacter$index
-     * contentCharacter is one of "1080p" "720" "blue-ray"
+     * @return
+     * return a single hash value if only one type of resource exists
+     * return up to 2 hash values if two options exists 1080p and 720p
      */
     private String animeSelector(String[][] resultList){
+        // first, calculate the average value of calculable items
+        // calculating the average size of all resources is useless
+
+        Integer[] seederList;
+        Integer count = 0;
+        int determination;
+        if (arrays.length >= 10){
+            determination = 10;
+            System.out.println("@@@@@@@@@@@@@");
+        } else {
+            determination = arrays.length-1;
+        }
+        System.out.println(arrays[10][5]);
+        for (int i = 0; i < determination ; i++) {
+            System.out.println(arrays[i][5]);
+        }
+        Integer seederAverage = count / resultList.length;
+        System.out.println(seederAverage);
+
+        System.out.println(arrays[10][5]);
+
+
         return "123";
     }
 
